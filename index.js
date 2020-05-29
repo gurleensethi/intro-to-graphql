236855f56b6e33af68c8216823536e0c70538aed
Gurleen Sethi <sarusethi@rocketmail.com>
2020-05-29 00:26:08 -0400

    feat: add product graphql and mongo type.

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
 const connection = require("./db/connection");
+const { ApolloServer } = require("apollo-server");
+const fs = require("fs");
+const path = require("path");
 
-(async () => {
+console.log(process.cwd());
+
+function loadSchemas() {
+  return ["coupon", "product", "user"]
+    .map((name) => {
+      const filePath = path.join(process.cwd(), "types", name, `${name}.gql`);
+      return fs.readFileSync(filePath).toString();
+    })
+    .join("\n");
+}
+
+const start = async () => {
   await connection.connectDB();
-})();
+
+  const schemas = loadSchemas();
+
+  const rootSchema = `
+    type Query
+    type Mutation      
+  `;
+
+  const server = new ApolloServer({
+    typeDefs: [rootSchema, schemas],
+    resolvers: {
+      Query: {},
+    },
+  });
+
+  const { url } = await server.listen({ port: 3000 });
+
+  console.log(`GraphlQL running on ${url}`);
+};
+
+try {
+  start();
+} catch (error) {
+  console.error(error);
+}
---
+++ types/product/product.model.js
@@ -0,0 +1,60 @@
+const { Schema, model, SchemaTypes } = require("mongoose");
+const validator = require("validator").default;
+
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+      default: "https://via.placeholder.com/150",
+      validate: [(v) => validator.isURL(v), "Not a valid image url"],
+    },
+    type: {
+      type: String,
+      required: true,
+      enum: ["GAMING_PC", "BIKE", "DRONE"],
+    },
+    description: String,
+    liquidCooled: {
+      type: Boolean,
+      required() {
+        return this.type === "GAMING_PC";
+      },
+    },
+    bikeType: {
+      type: String,
+      enum: ["KIDS", "MOUNTAIN", "ELECTRIC", "BEACH"],
+      required() {
+        return this.type === "BIKE";
+      },
+    },
+    range: {
+      type: String,
+      required() {
+        return this.type === "DRONE";
+      },
+    },
+    createdBy: {
+      type: SchemaTypes.ObjectId,
+      required: true,
+      ref: "user",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+model.exports = {
+  Product: model("product", productSchema),
+};
