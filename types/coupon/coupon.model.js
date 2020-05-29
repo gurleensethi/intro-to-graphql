ca3efc20d27492a0f4012220af2f37a433616b56
Gurleen Sethi <sarusethi@rocketmail.com>
2020-05-28 20:44:29 -0400

    feat: model and graphql type for coupon.

---
+++ types/coupon/coupon.model.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require("mongoose");
+
+const couponSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    code: {
+      type: String,
+      required: true,
+      uppercase: true,
+      trim: true,
+      unique: true,
+      minlength: 6,
+      maxlength: 6,
+    },
+    discount: {
+      type: Number,
+      max: 99,
+      min: 1,
+      required: true,
+    },
+    expiresOn: {
+      type: Date,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = model("coupon", couponSchema);
